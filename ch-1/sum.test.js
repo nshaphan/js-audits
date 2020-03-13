c3ea20401c727df5a36ab5868a74c6f15e3e9142
Charles Odili <chaluwa@gmail.com>
2020-03-13 20:16:06 +0100

    add tests

---
+++ ch-1/sum.test.js
@@ -0,0 +1,21 @@
+import sum from '../src/sum';
+
+describe('does basic addition', () => {
+  test('correctly adds 2 +ve numbers', () => {
+    expect(sum(2, 3)).toBe(5);
+    expect(sum(5, 4)).toBe(9);
+    expect(sum(10, 5)).toBe(15);
+  });
+
+  test('correctly adds 2 -ve numbers', () => {
+    expect(sum(-2, -3)).toBe(-5);
+    expect(sum(-5, -4)).toBe(-9);
+    expect(sum(-10, -5)).toBe(-15);
+  });
+
+  test('correctly adds +ve & -ve numbers', () => {
+    expect(sum(2, -3)).toBe(-1);
+    expect(sum(-5, 8)).toBe(3);
+    expect(sum(-10, 5)).toBe(-5);
+  });
+});
---
+++ ch-3/sum.test.js
@@ -0,0 +1,9 @@
+import sum from '../src/sum';
+
+describe('secures the bag', () => {
+  test('can handle edge cases', () => {
+    expect(sum()).toBe(0);
+    expect(sum(3)).toBe(3);
+    expect(sum(1, 2, -1, '3', null, {}, [], 4, ' 5 ', 1, ' ', undefined, 6)).toBe(21);
+  });
+});
