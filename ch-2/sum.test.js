ab05c8740272ff372bd71008cc7b44d4028cafcb
Charles Odili <chaluwa@gmail.com>
2020-03-16 00:12:37 +0100

    minify unit tests

--- ch-2/sum.test.js
+++ ch-2/sum.test.js
@@ -1,16 +1,1 @@
-import sum from '../../src/sum';
-
-describe('adds more than 2 numbers', () => {
-  test('correctly adds more than 2 +ve numbers', () => {
-    expect(sum(5, 2, 3)).toBe(10);
-  });
-
-  test('correctly adds more than 2 -ve numbers', () => {
-    expect(sum(-15, -7, -3)).toBe(-25);
-  });
-
-  test('correctly adds more than 2 +ve & -ve numbers', () => {
-    expect(sum(-15, 7, 3)).toBe(-5);
-    expect(sum(10, -5, 2, -11, 10)).toBe(6);
-  });
-});
+import sum from"../../src/sum";describe("adds more than 2 numbers",()=>{test("correctly adds more than 2 +ve numbers",()=>{expect(sum(5,2,3)).toBe(10)}),test("correctly adds more than 2 -ve numbers",()=>{expect(sum(-15,-7,-3)).toBe(-25)}),test("correctly adds more than 2 +ve & -ve numbers",()=>{expect(sum(-15,7,3)).toBe(-5),expect(sum(10,-5,2,-11,10)).toBe(6)})});
--- ch-3/sum.test.js
+++ ch-3/sum.test.js
@@ -1,9 +1,1 @@
-import sum from '../../src/sum';
-
-describe('secures the bag', () => {
-  test('can handle edge cases', () => {
-    expect(sum()).toBe(0);
-    expect(sum(3)).toBe(3);
-    expect(sum(1, 2, -1, '3', null, {}, [], 4, ' 5 ', 1, ' ', undefined, 6)).toBe(21);
-  });
-});
+import sum from"../../src/sum";describe("secures the bag",()=>{test("can handle edge cases",()=>{expect(sum()).toBe(0),expect(sum(3)).toBe(3),expect(sum(1,2,-1,"3",null,{},[],4," 5 ",1," ",void 0,6)).toBe(21)})});
